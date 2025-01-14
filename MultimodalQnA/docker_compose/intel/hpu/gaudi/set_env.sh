ce1e3c1a73b9f40fcc6087ee1c2ed1210cbe3b3b
okhleif-IL <omar.khleif@intel.com>
2025-01-14 13:44:19 -0800

    added back accidentally removed var
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>

--- MultimodalQnA/docker_compose/intel/hpu/gaudi/set_env.sh
+++ MultimodalQnA/docker_compose/intel/hpu/gaudi/set_env.sh
@@ -23,6 +23,7 @@ export REDIS_URL="redis://${host_ip}:${REDIS_DB_PORT}"
 export REDIS_HOST=${host_ip}
 export INDEX_NAME="mm-rag-redis"
 
+export WHISPER_MODEL="base"
 export WHISPER_PORT=7066
 export WHISPER_SERVER_ENDPOINT="http://${host_ip}:${WHISPER_PORT}/v1/asr"
 export MAX_IMAGES=1
